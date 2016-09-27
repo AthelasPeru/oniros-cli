30ebcfafe11ccc2d5e7fd60637004e888ce7afc3
ondoheer <ondoheer@gmail.com>
2016-09-27 09:06:52 -0500

    changed package name and versioning system

--- README.md
+++ README.md
@@ -2,5 +2,13 @@
 Oniros WP Theme builder CLI npm package
 
 ## Usage
-Just run ```oniros``` and the options will be displayed
+To install the oniros client globally you need to:
+```
+$ cd node_modules/oniros-cli
+$ sudo npm link
+$ cd ../.. #to return to your project root
+
+
+
+then just run ```oniros``` and the options will be displayed
 
--- bin/oniros.js
+++ bin/oniros.js
@@ -3,52 +3,20 @@ console.log("\n#####################");
 console.log("# Welcome to Oniros #");
 console.log("#####################\n");
 
-var shell = require("shelljs");
-var inquirer = require("inquirer");
 
+var menu = require("./menu.js");
 
-var choices = [
-	{
-		name:"1) Watch Main",
-		value: "watch:main"
-	},
-	{
-		name:"2) Watch All",
-		value: "watch:all"
-	},
-	{
-		name:"3) Build Main",
-		value: "build:main:all"
-	},
-	{
-		name: "4) Build All",
-		value: "build:all"
-	},
-	{
-		name: "5) Process Favicon",
-		value: "favicon"
-	},
-	{
-		name: "6) Minimize Images",
-		value: "imagemin"
-	},
-	{
-		name: "7) Build Login CSS",
-		value: "login:build:css"
-	},
-	{
-		name: "8) Zip Theme",
-		value: "zip"
-	}
-];
-var input = inquirer.prompt([
+menu.display([
 	{
 	    type: 'list',
 	    name: 'script',
 	    message: 'What do you want Oniros to do for you?',
 	    paginated: true,
-	    choices: choices
-  },
+	    choices: menu.choices
+  	},
 ]).then(function (answers) {
 	shell.exec("npm run " + answers.script);  
 });
+
+
+
